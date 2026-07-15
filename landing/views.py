fc782ec9bb48b5ed00fc12a234df4d66855f43d0
gustavoalb <gustavoo.alb2@gmail.com>
2026-07-14 23:22:49 -0500

    refactor: encapsular acesso a queryset em métodos seguros para evitar erros de operação

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.db.utils import OperationalError
 
 from core.models import SiteConfig
 from experiences.models import WorkExperience
@@ -11,21 +12,35 @@ class LandingPageView(TemplateView):
 
     template_name = 'landing/index.html'
 
+    def _safe_first(self, queryset):
+        try:
+            return queryset.first()
+        except OperationalError:
+            return None
+
+    def _safe_list(self, queryset):
+        try:
+            return list(queryset)
+        except OperationalError:
+            return []
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         # Configuração do site (singleton)
-        context['config'] = SiteConfig.objects.first()
+        context['config'] = self._safe_first(SiteConfig.objects.all())
 
         # Experiências ativas, ordenadas
-        context['experiences'] = WorkExperience.objects.filter(ativo=True)
+        context['experiences'] = self._safe_list(
+            WorkExperience.objects.filter(ativo=True)
+        )
 
         # Tecnologias ordenadas
-        context['skills'] = Skill.objects.all()
+        context['skills'] = self._safe_list(Skill.objects.all())
 
         # Projetos ativos, ordenados, com imagens pré-carregadas
-        context['projects'] = Project.objects.filter(
-            ativo=True
-        ).prefetch_related('imagens')
+        context['projects'] = self._safe_list(
+            Project.objects.filter(ativo=True).prefetch_related('imagens')
+        )
 
         return context
